987b9a9ccb161d0ee5e48632a7169f54d1212b92
Jet Kingston <jettaikingston10@gmail.com>
2026-05-14 19:56:07 +0200

    [addition]<difficulty mode created>

---
+++ WILL_Game3D/Assets/Scripts/Menu/ButtonHandler.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void OnEasyButtonClick()
+    {
+        Debug.Log("Easy Button Clicked!");
+        SceneManager.LoadScene("Easy");
+    }
+    public void OnMediumButtonClick()
+    {
+        Debug.Log("Medium Button Clicked!");
+        SceneManager.LoadScene("Medium");
+    }
+    public void OnHardButtonClick()
+    {
+        Debug.Log("Hard Button Clicked!");
+        SceneManager.LoadScene("Hard");
+    }
+}
